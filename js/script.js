355283f2197917b6c927a77f3b316a5d674b2ca0
MaxPredathor <max.gilli98@gmail.com>
2023-10-19 14:37:09 +0200

    added function that creates bombs

--- js/script.js
+++ js/script.js
@@ -9,7 +9,10 @@ function myCode(){
 
     selector.addEventListener('change', function(){
 
+
         let numeroBoxes = selector.value;
+        const NUM_BOMBS = 16;
+        let bombs = bombCreation();
 
         btn.addEventListener('click', function(){
 
@@ -27,15 +30,37 @@ function myCode(){
             let radiceQ = Math.sqrt(numeroBoxes);
             myBox.style.width = `calc(100% / ${radiceQ})`;
             myBox.style.height = `calc(100% / ${radiceQ})`;
-            myBox.addEventListener('click', function(){
+            myBox.addEventListener('click', function clickMe(){
                 console.log(this.innerText);
-                myBox.classList.add('onclick');
+
+
+                if(bombs.includes(parseInt(myBox.innerText))){
+                    myBox.classList.add('bomb');
+                }else{
+                    myBox.classList.add('onclick');
+                }
+                
                 myBox.style.color = 'black';
                 myBox.style.fontWeight = 'bold';
+                myBox.removeEventListener('click', clickMe);
 
             });
             return myBox;
     
+        }
+
+        function bombCreation(){
+            let bombArray = [];
+            while(bombArray.length <= NUM_BOMBS - 1){
+                let num = getRndInteger(1, numeroBoxes);
+                if(bombArray.includes(num)){
+
+                }else{
+                    bombArray.push(num);
+                }
+            }
+            console.log(bombArray.sort());
+            return bombArray;
         }
 
     })  
